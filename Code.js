28debca29c22a691783b190ee90ebb12f50dd5b7
DaGoatly <77469938+DaGoatly@users.noreply.github.com>
2021-05-20 20:00:20 -0400

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+    fill("white")   
+    textSize(15)
+    text("T U C F I N N O", 700,150)
+    fill("lightblue")
+    text("Hint: Tells the script to do something !!", 700,170)
+
+    fill("white")   
+    textSize(15)
+    text("T A A D A E S B", 100,250)
+    fill("lightblue")
+    text("Hint: Stores all the information !!", 100,270)
+
+}
